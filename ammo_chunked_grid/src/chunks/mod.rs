8ebc95828e9fe4f64c5c8c355cc3c9d49f49e977
Austin Hicks <ahicks@ahicks.io>
2021-05-27 18:30:40 -0700

    Clean up ammo_chunked_grid chunks

--- ammo_chunked_grid/src/chunks/mod.rs
+++ ammo_chunked_grid/src/chunks/mod.rs
@@ -1,4 +1,9 @@
-//! Chunks are encoded as a series of m*n boxes which are allocated inline on demand.
+//! Chunks are encoded as a series of small subarrays allocated on demand in a vec, e.g.
+//! `[[a, b, c, d], [a, b, c, d]]`, which is indexed with a linear index computed by converting
+//! `(x, y)` pairs using `y * CHUNK_WIDTH + x`.  The nested subarrays are looked up using an indirection table, so that they can be allocated on demand and so
+//! that we can reserve a default value at the beginning of the vec.  This gives a branchless read.  It also gives a
+//?! mostly branchless write, save that write needs to check whether or not the subarray in question
+//! is pointing at the default value or a writable subarray.
 
 // We need these constants because Rust const generics isn't yet powerful enough
 // to be abstract over the dimensions of the cells.
@@ -6,34 +11,33 @@
 /// Should evenly divide u64::MAX.
 pub(crate) const CHUNK_WIDTH: usize = 128;
 pub(crate) const CHUNK_HEIGHT: usize = 128;
-pub(crate) const BOX_WIDTH: usize = 2;
-pub(crate) const BOX_HEIGHT: usize = 2;
-const TOTAL_BOXES: usize = CHUNK_WIDTH * CHUNK_HEIGHT / BOX_WIDTH / BOX_HEIGHT;
+/// Should evenly divide the chunk.
+const SUBARRAY_LEN: usize = 4;
+const TOTAL_SUBARRAYS: usize = CHUNK_WIDTH * CHUNK_HEIGHT / SUBARRAY_LEN;
 
 pub(crate) struct Chunk {
-    /// The data of the chunk. Subdivided into small `BOX_WIDTH * BOX_HEIGHT`
-    /// subarrays, but with the first index always being the default value of
-    /// the chunk, so that looking up boxes through the indirection table can be
-    /// branchless.
+    /// Data of the chunk.  The first index is the default value for the chunk.
+    /// All indices after that are the subarrays in groups of `SUBARRAY_LEN`.
+    /// Put another way, the vec is always `1 + SUBARRAY_LEN * n` elements long.
     data: Vec<u32>,
-    /// The offsets of the boxes, using `0` as an unallocated box.
-    box_offsets: [u16; TOTAL_BOXES],
+    /// the offsets of the subarrays, using 0 as a sentinel value that instead means default value.
+    subarray_offsets: [u16; TOTAL_SUBARRAYS],
 }
 
 #[derive(Debug)]
-struct BoxRef {
-    /// Index of the box in the lookup table.
-    box_index: usize,
-    /// Index to read/write relative to the box's start for given coordinates.
+struct SubarrayRef {
+    /// Index of the subarray in the lookup table.
+    subarray_index: usize,
+    /// Index to read/write relative to the subarray's start.
     data_index: usize,
 }
 
-fn coords_to_box(x: usize, y: usize) -> BoxRef {
+fn coords_to_subarray(x: usize, y: usize) -> SubarrayRef {
     let linear_index = y * CHUNK_WIDTH + x;
-    let box_index = linear_index / (BOX_WIDTH * BOX_HEIGHT);
-    let data_index = linear_index - box_index * BOX_WIDTH * BOX_HEIGHT;
-    BoxRef {
-        box_index,
+    let subarray_index = linear_index / SUBARRAY_LEN;
+    let data_index = linear_index - subarray_index * SUBARRAY_LEN;
+    SubarrayRef {
+        subarray_index,
         data_index,
     }
 }
@@ -42,32 +46,30 @@ impl Chunk {
     pub(crate) fn new(default_val: u32) -> Chunk {
         Chunk {
             data: vec![default_val],
-            box_offsets: [0; TOTAL_BOXES],
+            subarray_offsets: [0; TOTAL_SUBARRAYS],
         }
     }
 
     pub(crate) fn read(&self, x: usize, y: usize) -> u32 {
-        let box_data = coords_to_box(x, y);
-        let arr_off = self.box_offsets[box_data.box_index];
-        // We need to account for boxes that point at defaults and don't want
-        // branches. Let's convert to anint between 0 and 1 and multiply.
+        let arr_data = coords_to_subarray(x, y);
+        let arr_off = self.subarray_offsets[arr_data.subarray_index];
+        // We need to account for subarrays that point at defaults and don't want
+        // branches. Let's convert to an int between 0 and 1 and multiply.
         let mul = (arr_off != 0) as usize;
-        self.data[arr_off as usize + box_data.data_index * mul]
+        self.data[arr_off as usize + arr_data.data_index * mul]
     }
 
     /// Write to the cell, returning the old value.
     pub(crate) fn write(&mut self, x: usize, y: usize, value: u32) -> u32 {
-        let box_info = coords_to_box(x, y);
-        //println!("{:?}", box_info);
+        let arr_info = coords_to_subarray(x, y);
         let defval = self.data[0];
-        let mut box_ind = self.box_offsets[box_info.box_index];
-        if box_ind == 0 {
-            self.data
-                .resize(self.data.len() + BOX_WIDTH * BOX_HEIGHT, defval);
-            box_ind = (self.data.len() - BOX_WIDTH * BOX_HEIGHT) as u16;
-            self.box_offsets[box_info.box_index] = box_ind;
+        let mut arr_ind = self.subarray_offsets[arr_info.subarray_index];
+        if arr_ind == 0 {
+            self.data.resize(self.data.len() + SUBARRAY_LEN, defval);
+            arr_ind = (self.data.len() - SUBARRAY_LEN) as u16;
+            self.subarray_offsets[arr_info.subarray_index] = arr_ind;
         }
-        let dest = &mut self.data[box_ind as usize + box_info.data_index];
+        let dest = &mut self.data[arr_ind as usize + arr_info.data_index];
         let old = *dest;
         *dest = value;
         old
