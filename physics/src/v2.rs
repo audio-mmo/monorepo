82317d701c15f5d5e727214c597f9bd57ec3b76a
Austin Hicks <ahicks@ahicks.io>
2021-07-20 17:36:13 -0700

    Length/normalization helper functions for v2

--- physics/src/v2.rs
+++ physics/src/v2.rs
@@ -10,4 +10,21 @@ impl V2 {
     pub fn new(x: f64, y: f64) -> V2 {
         V2 { x, y }
     }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    #[must_use = "This function doesn't modify the vector in place"]
+    pub fn normalize(self) -> V2 {
+        let l = self.length();
+        V2 {
+            x: self.x / l,
+            y: self.y / l,
+        }
+    }
 }
