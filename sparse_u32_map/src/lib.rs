2cf81e927036391764f4b3858173933b30d51cd4
Austin Hicks <ahicks@ahicks.io>
2021-08-22 18:25:56 -0700

    SparseU32Map: reimplement using a boring HashSet
    Because wasting huge chunks of empty memory for a few thousand objects is not
    going to work.  As the comment says, we can optimize later, say by using a
    custom hashmap optimized for u32 keys (something we probably write anyway, to
    be honest).

--- sparse_u32_map/src/lib.rs
+++ sparse_u32_map/src/lib.rs
@@ -3,83 +3,33 @@
 //! - Iteration is `O(n)` and as fast as any vec or slice.
 //! - Member checks are `O(1)`.
 //! - The map iterates in insertion order as long as no deletes are performed,
-//!   and iteration of items added after the last delete are visited in sorted
+//!   and iteration of items added after the last delete are visited in insertion
 //!   order.
-use std::alloc::{alloc, Layout};
-use std::ptr::NonNull;
+use std::collections::HashMap;
 
-// Must be a power of 2.
-const PAGE_SIZE: usize = 1 << 15;
-const PAGE_MASK: usize = !(PAGE_SIZE - 1);
-
-#[inline(always)]
-fn key_to_page_index(key: u32) -> (usize, usize) {
-    let ind = key as usize;
-    (ind & PAGE_MASK, ind & !PAGE_MASK)
-}
-
-unsafe fn alloc_page() -> *mut u32 {
-    let layout = Layout::array::<u32>(PAGE_SIZE).unwrap();
-    alloc(layout) as *mut u32
-}
-
-struct Page {
-    inner: NonNull<u32>,
-}
-
-impl Page {
-    fn new() -> Page {
-        Page {
-            inner: unsafe { NonNull::new_unchecked(alloc_page()) },
-        }
-    }
-
-    fn read(&self, index: usize) -> u32 {
-        debug_assert!(index < PAGE_SIZE);
-        unsafe { *self.inner.as_ptr().add(index) }
-    }
-
-    fn write(&mut self, index: usize, value: u32) {
-        debug_assert!(index < PAGE_SIZE);
-        unsafe { *self.inner.as_ptr().add(index) = value };
-    }
-}
-
-impl Drop for Page {
-    fn drop(&mut self) {
-        unsafe {
-            std::alloc::dealloc(
-                self.inner.as_ptr() as *mut u8,
-                Layout::array::<u32>(PAGE_SIZE).unwrap(),
-            );
-        }
-    }
-}
-
-impl Clone for Page {
-    fn clone(&self) -> Page {
-        unsafe {
-            let np = alloc_page();
-
-            std::ptr::copy_nonoverlapping(self.inner.as_ptr(), np, PAGE_SIZE);
-            Self {
-                inner: NonNull::new_unchecked(np),
-            }
-        }
-    }
-}
+type Hasher = std::hash::BuildHasherDefault<rustc_hash::FxHasher>;
 
 // See: https://research.swtch.com/sparse
 //
 // The basic idea is that we can check for an element in the set by asking
 // `sparse[i]` where it thinks the element may be at, then comparing the element
 // in the set.
+//
+// But we have a twist.  Ammo wants to actually use small sets per area, not one
+// big set for the entire loaded map.  As a consequence, we actually use
+// `HashMap` to back the indices (not the cached version; we don't expect this
+// to use the same key repeatedly).  The uninitialized memory trick works great
+// for the case wherein there's a lot of ids, but allocating large pages wastes
+// something on the order of 90%+ memory.  We can accelerate this if we need to
+// by implementing a custom hashtable, but we care about fast iteration
+// performance and we still have that, so punt optimizing down the road since it
+// can definitely be done once it becomes necessary.
 
 #[derive(Clone)]
 pub struct SparseU32Map<V> {
     dense_keys: Vec<u32>,
     dense_values: Vec<V>,
-    sparse_keys: Vec<Option<Page>>,
+    sparse_keys: HashMap<u32, u32, Hasher>,
 }
 
 impl<V> Default for SparseU32Map<V> {
@@ -102,12 +52,7 @@ impl<V> SparseU32Map<V> {
     }
 
     fn dense_index_for_key(&self, key: u32) -> Option<usize> {
-        let (page, index) = key_to_page_index(key);
-        let ind = self
-            .sparse_keys
-            .get(page)
-            .and_then(|x| Some(x.as_ref()?.read(index)))
-            .unwrap_or(0) as usize;
+        let ind = self.sparse_keys.get(&key).cloned().unwrap_or(0) as usize;
         if self.dense_keys.get(ind) == Some(&key) {
             Some(ind)
         } else {
@@ -129,23 +74,8 @@ impl<V> SparseU32Map<V> {
         self.dense_values.get_mut(ind)
     }
 
-    fn ensure_page(&mut self, page: usize) -> &mut Page {
-        self.sparse_keys
-            .resize(self.sparse_keys.len().max(page + 1), None);
-        match self.sparse_keys.get_mut(page) {
-            Some(Some(p)) => &mut *p,
-            Some(x) => {
-                *x = Some(Page::new());
-                x.as_mut().unwrap()
-            }
-            _ => unsafe { std::hint::unreachable_unchecked() },
-        }
-    }
-
-    /// Returns the old value if any.r
+    /// Returns the old value if any.
     pub fn insert(&mut self, key: u32, value: V) -> Option<V> {
-        let (page, page_ind) = key_to_page_index(key);
-
         let new_dense_ind = match self.dense_index_for_key(key) {
             Some(ind) => {
                 self.dense_keys[ind] = key;
@@ -160,30 +90,26 @@ impl<V> SparseU32Map<V> {
             }
         };
 
-        let page = self.ensure_page(page);
-        page.write(page_ind, new_dense_ind as u32);
+        self.sparse_keys.insert(key, new_dense_ind as u32);
         None
     }
 
-    fn swap(&mut self, first: usize, second: usize) {
+    /// Unsafe because it performs UB if the indices are out of bounds.
+    unsafe fn swap(&mut self, first: usize, second: usize) {
         self.dense_keys.swap(first, second);
         self.dense_values.swap(first, second);
-        let (p1, i1) = key_to_page_index(self.dense_keys[first]);
-        let (p2, i2) = key_to_page_index(self.dense_keys[second]);
-        if p1 == p2 {
-            let page = self.ensure_page(p1);
-            page.write(i1, first as u32);
-            page.write(i2, second as u32);
-        } else {
-            self.ensure_page(p1).write(i1, first as u32);
-            self.ensure_page(p2).write(i2, second as u32);
-        }
+        self.sparse_keys
+            .insert(*self.dense_keys.get_unchecked(first), first as u32);
+        self.sparse_keys
+            .insert(*self.dense_keys.get_unchecked(second), second as u32);
     }
 
     pub fn remove(&mut self, key: u32) -> Option<V> {
         let dense_ind = self.dense_index_for_key(key)?;
         let end_ind = self.dense_keys.len() - 1;
-        self.swap(dense_ind, end_ind);
+        unsafe {
+            self.swap(dense_ind, end_ind);
+        };
         match self.dense_keys.pop() {
             Some(_) => (),
             None => unsafe { std::hint::unreachable_unchecked() },
