5c7229e33802b749bf1497717a74cf515cfd303f
Austin Hicks <ahicks@ahicks.io>
2021-10-15 13:06:04 -0700

    Begin simplifying the ObjectId story

--- ecs/src/object_id.rs
+++ ecs/src/object_id.rs
@@ -1,82 +1,45 @@
 //! The [ObjectId] type.
 //!
-//! Objects are represented by a 127-bit uniformly distributed random id, and a
-//! 1-bit ephemeral flag.  This is then split across 2 u64 fields, because while
-//! Rust supports 128-bit integers, these ids are their own 64-bit hashes.
+//! Object ids are composed of a 64-bit counter which monotonically increments per run of the program, and a 64-bit
+//! random value.  In practice, the counter is the number of nanoseconds since the Unix epoch, but this detail is
+//! handled by the constructor of the object id and may not hold true for tests. The intent is that the counters
+//! (almost) never repeat, and the 64-bit random value is used as a low-quality uuid.
 //!
-//! Layout is `(ephemeral, upper, lower)` where ephemeral is the most
-//! significant bit of upper.
+//! In order to play nice with niche value optimizations, the counter must never be zero.
 //!
-//! Additionally upper is a `NonZeroU64` so that this can work well with
-//! `Option`.  To make that work we specifically check for it and replace it
-//! with 1, removing exactly 1 value from the 127 bits of randomness.
-//!
-//! This is an odd design for an ECS, but is justified by 3 factors: first, we
-//! don't need to be as fast as other implementations (e.g. no "let's do
-//! particle systems where every particle is an entity").  Second, we need
-//! unique ids across the network.  Third, we nee unique ids across potentially
-//! realtime years.  Let's kill the complexity of versions and kill the
-//! complexity of some auxiliary system to reference objects and just take the
-//! slight performance hit.
+//! In addition to functioning as a tie-breaker to prevent uniqueness, the random component of the id can also be used
+//! as a hash.
 use std::num::NonZeroU64;
-
-/// Mask to extract the ephemeral bit.
-const EPHEMERAL_MASK: u64 = 1 << 63;
 
 #[derive(Copy, Clone, Eq, Ord, PartialEq, PartialOrd, Hash)]
 pub struct ObjectId {
-    upper: NonZeroU64,
-    lower: u64,
+    counter: NonZeroU64,
+    random: u64,
 }
 
 impl ObjectId {
-    pub(crate) fn new(ephemeral: bool, mut upper: u64, lower: u64) -> ObjectId {
-        upper &= !EPHEMERAL_MASK;
-        upper |= (ephemeral as u64) << 63;
-        if upper == 0 {
-            upper = 1;
-        }
-        unsafe {
-            Self {
-                upper: NonZeroU64::new_unchecked(upper),
-                lower,
-            }
+    pub fn new(counter: u64, random: u64) -> ObjectId {
+        assert!(counter != 0);
+        ObjectId {
+            counter: NonZeroU64::new(counter).expect("Counter must not be zero"),
+            random,
         }
     }
 
-    pub fn get_ephemeral_bit(&self) -> bool {
-        self.upper.get() & EPHEMERAL_MASK != 0
+    pub fn get_counter(&self) -> u64 {
+        self.counter.get()
+    }
+
+    pub fn get_random(&self) -> u64 {
+        self.random
     }
 }
 
 impl std::fmt::Debug for ObjectId {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
         fmt.debug_struct("ObjectId")
-            .field("ephemeral", &self.get_ephemeral_bit())
-            .field("upper", &(self.upper.get() & !EPHEMERAL_MASK))
-            .field("lower", &self.lower)
+            .field("counter", &self.counter.get())
+            .field("random", &self.random)
             .finish()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn ephemeral() {
-        let not_ephemeral = ObjectId::new(false, 1, 2);
-        assert_eq!(not_ephemeral.upper.get(), 1);
-        assert_eq!(not_ephemeral.lower, 2);
-        let ephemeral = ObjectId::new(true, 1, 2);
-        assert_eq!(ephemeral.upper.get(), 0x8000000000000001);
-        assert_eq!(ephemeral.lower, 2);
-    }
-
-    #[test]
-    fn no_accidental_ephemeral() {
-        let oid = ObjectId::new(false, 1 << 63, 2);
-        assert_eq!(oid.upper.get(), 1);
-        assert_eq!(oid.lower, 2);
     }
 }
