b54a8e4758c7ad27e633fddfbbfaa30ac4a0f625
Austin Hicks <ahicks@ahicks.io>
2021-10-16 10:37:08 -0700

    Finish the API for the store. Still not tested.

--- ecs/src/store.rs
+++ ecs/src/store.rs
@@ -1,7 +1,4 @@
 #![allow(dead_code)]
-//! Implements a vector-backed storage keyed by `ObjectId` intended for use with components.
-//!
-//! This isn't limited to components because it can store anything.
 use std::cell::UnsafeCell;
 use std::collections::BTreeMap;
 
@@ -9,26 +6,6 @@ use bitvec::vec::BitVec;
 
 use crate::object_id::ObjectId;
 
-/// A `Store` is like a combinationb between an array and a map: it is possible to ask about specific indices, as well
-/// as specific keys.
-///
-/// Internally, this is a set of 3 arrays: an array of object id keys, an array of values, and an array of tombstones.
-/// The tombstones are toggled on when objects are deleted rather than moving objects so that it is possible to iterate
-/// over the store while deleting.  The map isn't threadsafe and uses interior mutability, so that it is possible to
-/// iterate over it while performing mutable operations.  Most operations panic if called in a way which isn't safe,
-/// e.g. using indices past the end.
-///
-/// `maintenance` should periodically be called in order to clear out tombstones and actually drop values.  Otherwise
-/// the map never reclaims space.  Until `maintenance` is called, indices are stable on deletion.
-///
-/// Indices are stable on insertion, but newly inserted items may not be visible until `commit_inserts` or `maintenance`
-/// is called.  Inserts which replace a value for an item already in the store will always be immediately visible.
-///
-/// Iteration is always in order by increasing id, which is the primary invariant we wish to maintain: it allows for
-/// efficient merges.  This is why the other invariants and usage patterns are slightly awkward: without compromises,
-/// this can only be done with very frequent sorting.
-///
-/// This container is highly optimized for mostly sorted data.
 pub struct Store<T> {
     state: UnsafeCell<StoreState<T>>,
 }
@@ -90,6 +67,18 @@ impl<T> StoreState<T> {
 
         self.tombstones.truncate(self.keys.len());
         self.tombstones.set_elements(0);
+    }
+
+    fn maintenance(&mut self) {
+        self.commit_pending_inserts();
+        self.keys.shrink_to_fit();
+        self.values.shrink_to_fit();
+        self.tombstones.shrink_to_fit();
+    }
+
+    /// Public wrapper of binary_search from std.
+    fn binary_search(&self, id: &ObjectId) -> Result<usize, usize> {
+        self.keys.binary_search(id)
     }
 
     fn search_index_from_id(&self, id: &ObjectId) -> SearchResult {
@@ -177,7 +166,7 @@ impl<T> StoreState<T> {
         assert_eq!(self.values.len(), self.tombstones.len());
     }
 
-    fn get_by_id(&mut self, id: &ObjectId) -> Option<&T> {
+    fn get_by_id(&self, id: &ObjectId) -> Option<&T> {
         match self.search_index_from_id(id) {
             SearchResult::Found(i) => self.values.get(i),
             SearchResult::TombstoneAvailabel(_) | SearchResult::InsertBefore(_) => None,
@@ -200,6 +189,48 @@ impl<T> StoreState<T> {
     fn get_by_index_mut(&mut self, index: usize) -> Option<&mut T> {
         self.values.get_mut(index)
     }
+
+    fn index_for_id(&self, id: &ObjectId) -> Option<usize> {
+        match self.search_index_from_id(id) {
+            SearchResult::Found(i) => Some(i),
+            _ => None,
+        }
+    }
+
+    fn delete_id(&mut self, id: &ObjectId) -> bool {
+        match self.search_index_from_id(id) {
+            SearchResult::Found(i) => {
+                self.tombstones.set(i, true);
+                true
+            }
+            SearchResult::Pending => {
+                self.pending_inserts
+                    .remove(id)
+                    .expect("Should havve been in pending inserts");
+                true
+            }
+            SearchResult::InsertBefore(_) | SearchResult::TombstoneAvailabel(_) => false,
+        }
+    }
+
+    fn delete_index(&mut self, index: usize) -> bool {
+        let ret = self.tombstones[index];
+        self.tombstones.set(index, true);
+        // Ret is true if there was already a tombstone, e.g. we did nothing.
+        !ret
+    }
+
+    fn is_index_alive(&self, index: usize) -> bool {
+        !self.tombstones[index]
+    }
+
+    fn index_len(&self) -> usize {
+        self.keys.len()
+    }
+
+    fn id_at_index(&self, index: usize) -> ObjectId {
+        self.keys[index]
+    }
 }
 
 impl<T> Store<T> {
@@ -219,5 +250,137 @@ impl<T> Store<T> {
     }
 
     /// Commit a batch of inserts.
-    fn commit_inserts(&mut self) {}
+    fn commit_pending_inserts(&mut self) {
+        self.with_state(|s| s.commit_pending_inserts())
+    }
+
+    /// Perform maintenance. Commits inserts which are outstanding and shrinks the internal arrays to reclaim space.
+    fn maintenance(&mut self) {
+        self.with_state(|s| s.maintenance())
+    }
+
+    fn insert(&self, key: &ObjectId, val: T) -> Option<T> {
+        self.with_state(|s| s.insert(key, val))
+    }
+
+    pub fn get_by_id(&self, id: &ObjectId) -> Option<&T> {
+        // We use the fact that only &mut methods move things around to prove safety.
+        unsafe { (&*self.state.get()).get_by_id(id) }
+    }
+
+    pub fn get_by_id_mut(&mut self, id: &ObjectId) -> Option<&mut T> {
+        unsafe { (&mut *self.state.get()).get_by_id_mut(id) }
+    }
+
+    // Read a particular index. Don't check the tombstone.
+    pub fn get_by_index(&self, index: usize) -> Option<&T> {
+        unsafe { (&*self.state.get()).get_by_index(index) }
+    }
+
+    /// Read a particular index. Don't check the tombstone.
+    pub fn get_by_index_mut(&mut self, index: usize) -> Option<&mut T> {
+        unsafe { (&mut *self.state.get()).get_by_index_mut(index) }
+    }
+
+    /// Is the specified index alive?
+    pub fn is_index_alive(&self, index: usize) -> bool {
+        self.with_state(|s| s.is_index_alive(index))
+    }
+
+    /// Find the index for a particular object id. Returns `Some(index)` if the object is in the store and is a
+    /// committed insert.
+    pub fn index_for_id(&self, id: &ObjectId) -> Option<usize> {
+        self.with_state(|s| s.index_for_id(id))
+    }
+
+    /// Return the length of the index portion.
+    pub fn index_len(&self) -> usize {
+        self.with_state(|s| s.index_len())
+    }
+
+    fn id_at_index(&self, index: usize) -> ObjectId {
+        self.with_state(|s| s.id_at_index(index))
+    }
+
+    fn binary_search(&self, id: &ObjectId) -> Result<usize, usize> {
+        self.with_state(|s| s.binary_search(id))
+    }
+}
+
+pub struct StoreVisitor<T> {
+    last_seen_id: Option<ObjectId>,
+    last_index: usize,
+    pd: std::marker::PhantomData<*const T>,
+}
+
+impl<T> StoreVisitor<T> {
+    pub fn new(#[allow(unused_variables)] store: &Store<T>) -> StoreVisitor<T> {
+        StoreVisitor {
+            last_index: 0,
+            last_seen_id: None,
+            pd: Default::default(),
+        }
+    }
+
+    fn advance_past_tombstones(&mut self, store: &Store<T>) {
+        while !store.is_index_alive(self.last_index) {
+            self.last_index += 1;
+        }
+    }
+
+    /// Advance the index.
+    ///
+    /// This deals with object deletion, as well as index shifts: we compare against the last object id, and binary
+    /// search to find the first thing after it when we detect a mismatch.
+    fn incr_index(&mut self, store: &Store<T>) {
+        if self.last_index >= store.index_len() {
+            return;
+        }
+
+        // if we have a last seen id, sanity check it, then bump by one.
+        if let Some(ref oid) = self.last_seen_id {
+            if store.id_at_index(self.last_index) == *oid {
+                // Our index is good. just increment it.
+                self.last_index += 1;
+            }
+            // Otherwise, we need to find the nearest index to the object and base it off that.
+            self.last_index = match store.binary_search(oid) {
+                Ok(i) => i + 1,
+                // We didn't find it, but we have the index to insert before; this is our next index.
+                Err(i) => i,
+            };
+        }
+        // Otherwise we haven't advanced yet.
+
+        self.advance_past_tombstones(store);
+
+        // Our last seen id is the index we're currently at, if this is possible to get.
+        if self.last_index < store.index_len() {
+            self.last_seen_id = Some(store.id_at_index(self.last_index));
+        }
+    }
+
+    pub fn next<'a>(&mut self, store: &'a Store<T>) -> Option<(ObjectId, &'a T)> {
+        self.incr_index(store);
+        if self.last_index >= store.index_len() {
+            return None;
+        }
+
+        return Some((
+            store.id_at_index(self.last_index),
+            store.get_by_index(self.last_index).unwrap(),
+        ));
+    }
+
+    pub fn next_mut<'a>(&mut self, store: &'a mut Store<T>) -> Option<(ObjectId, &'a mut T)> {
+        self.incr_index(store);
+        if self.last_index >= store.index_len() {
+            return None;
+        }
+
+        Some((
+            store.id_at_index(self.last_index),
+            store.get_by_index_mut(self.last_index).unwrap(),
+        ))
+    }
 }
