e2a77b5e7c6a4cc68d8c619b33b682b1feae3789
Austin Hicks <ahicks@ahicks.io>
2021-12-31 07:33:22 -0800

    Frontend: make the menu create ListItems directly

--- ui_frontend/src/menu.py
+++ ui_frontend/src/menu.py
@@ -1,12 +1,10 @@
 import wx
 from cytolk import tolk
 
-
 class MenuItem:
     def __init__(self, text: str, value: object):
         self.text = text
         self.value = value
-
 
 class Menu:
     def __init__(self, parent, items: MenuItem, on_select):
@@ -17,8 +15,11 @@ class Menu:
         self.cancel_button = wx.Button(parent=self.panel, label="Cancel")
 
         self.list.InsertColumn(0, "")
-        for i in items:
-            self.list.Append([i.text])
+        for ind, i in enumerate(items):
+            li = wx.ListItem()
+            li.SetText(i.text)
+            li.SetId(ind)
+            self.list.InsertItem(li)
 
         self.ok_button.Bind(wx.EVT_BUTTON, self.on_ok, id=wx.ID_ANY)
         self.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.on_list_click, id=wx.ID_ANY)
