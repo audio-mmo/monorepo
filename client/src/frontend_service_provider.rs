7de3930e245b12240b546b14e41faa771dbb772f
Austin Hicks <ahicks@ahicks.io>
2021-12-31 13:58:49 -0800

    client: write the FrontendServiceProvider

---
+++ client/src/frontend_service_provider.rs
@@ -0,0 +1,57 @@
+//! Provides the code to let the client call into the frontend.
+use anyhow::Result;
+use crossbeam::channel as chan;
+
+use ammo_protos::frontend::{self, ServiceRequest};
+
+pub struct FrontendServiceProvider {
+    request_sender: chan::Sender<ServiceRequest>,
+    request_receiver: chan::Receiver<ServiceRequest>,
+}
+
+impl FrontendServiceProvider {
+    pub fn new() -> Self {
+        let (request_sender, request_receiver) = chan::unbounded();
+        Self {
+            request_sender,
+            request_receiver,
+        }
+    }
+
+    pub fn speak(&self, text: &str, interrupt: bool) -> Result<()> {
+        let command_payload = frontend::SpeakRequest {
+            interrupt,
+            text: text.to_string(),
+        };
+        let req: frontend::ServiceRequest = frontend::ServiceRequest {
+            speech: Some(command_payload),
+            ..Default::default()
+        };
+        self.request_sender.send(req)?;
+        Ok(())
+    }
+
+    pub fn shutdown(&self) -> Result<()> {
+        let cmd: ServiceRequest = ServiceRequest {
+            shutdown: Some(Default::default()),
+            ..Default::default()
+        };
+        self.request_sender.send(cmd)?;
+        Ok(())
+    }
+
+    /// Extracct all of the pending commands.
+    pub fn extract_requests(&self, dest: &mut Vec<ServiceRequest>) -> Result<()> {
+        while let Ok(r) = self.request_receiver.try_recv() {
+            dest.push(r);
+        }
+
+        Ok(())
+    }
+}
+
+impl Default for FrontendServiceProvider {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod frontend_service_provider;
 pub mod ui_elements;
 pub mod ui_stack;
 pub mod world_state;
