caf758860d087c5a32f9024dcef05bf18d10bd55
Austin Hicks <ahicks@ahicks.io>
2022-01-29 16:04:26 -0800

    net: strip some doc comments
    We're refactoring this a lot; let's not keep things that might get stale

--- net/src/authentication.rs
+++ net/src/authentication.rs
@@ -6,7 +6,6 @@ use ammo_framer::Message;
 
 use crate::Connection;
 
-/// Authenticate a connection, wiring it up to whatever it needs to be wired up to.
 pub trait Authenticator: Send + Sync + 'static {
     fn authenticate(&self, first_msg: &Message, conn: Arc<dyn Connection>) -> Result<()>;
 }
--- net/src/connection.rs
+++ net/src/connection.rs
@@ -2,15 +2,8 @@ use ammo_framer::Message;
 
 use anyhow::Result;
 
-/// Trait representing the ability to send and receive messages.
-///
-/// when dropped, the connection closes.
 pub trait Connection {
-    /// Read some messages from the connection, if any.
-    ///
-    /// Calls the callback on the messages, which opens us up to the ability to do zero-copy deserialization.
     fn receive_messages(&self, callback: &dyn FnMut(&Message) -> Result<()>) -> Result<()>;
 
-    /// Send a message.
     fn send_message(&self, message: &Message) -> Result<()>;
 }
