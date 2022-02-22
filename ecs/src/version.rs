83ab9f6226838dff144f69fc1422c55b5c3dea63
Austin Hicks <ahicks@ahicks.io>
2022-02-21 19:42:31 -0800

    Revert "ecs: make versions an i64 internally"
    This reverts commit ab81651abd9666c20863daa4d8328ca894595cdb.

--- ecs/src/version.rs
+++ ecs/src/version.rs
@@ -4,8 +4,6 @@
 /// process start.  Serializes as a u64.
 ///
 /// The default impl is the minimum possible version.
-///
-/// As with other things we might want to save to Sqlite, the internal representation is i64.
 #[derive(
     Copy,
     Clone,
@@ -14,17 +12,18 @@
     Eq,
     PartialOrd,
     Ord,
+    Default,
     serde::Serialize,
     serde::Deserialize,
     derive_more::Display,
 )]
 #[serde(transparent)]
 #[display(fmt = "v{_0}")]
-pub struct Version(i64);
+pub struct Version(u64);
 
 impl Version {
     /// The version less than all other versions.
-    pub const MIN: Version = Version(i64::MIN);
+    pub const MIN: Version = Version(0);
 
     /// Get the next version after this one.
     ///
@@ -33,11 +32,5 @@ impl Version {
     pub fn increment(&self) -> Version {
         let nv = self.0.checked_add(1).expect("We hit the max version!");
         Version(nv)
-    }
-}
-
-impl Default for Version {
-    fn default() -> Version {
-        Version::MIN
     }
 }
