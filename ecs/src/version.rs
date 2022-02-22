6aa5de3753950d7b043f1fb3f354d718f7c4f5a8
Austin Hicks <ahicks@ahicks.io>
2022-02-21 19:46:18 -0800

    ecs/version: use NonZeroU64 internally

--- ecs/src/version.rs
+++ ecs/src/version.rs
@@ -1,3 +1,5 @@
+use std::num::NonZeroU64;
+
 /// A version of something, usually a component.
 ///
 /// these are primarily used for the network and other change detection, where we consider all entities "changed" at
@@ -12,25 +14,37 @@
     Eq,
     PartialOrd,
     Ord,
-    Default,
     serde::Serialize,
     serde::Deserialize,
     derive_more::Display,
 )]
 #[serde(transparent)]
 #[display(fmt = "v{_0}")]
-pub struct Version(u64);
+pub struct Version(NonZeroU64);
 
 impl Version {
     /// The version less than all other versions.
-    pub const MIN: Version = Version(0);
+    pub const MIN: Version = Version(unsafe { NonZeroU64::new_unchecked(1) });
 
     /// Get the next version after this one.
     ///
     /// Panics if this isn't possible, which is a sign of a bug: we'd have to have a u64::MAX -1 version.
     #[must_use]
     pub fn increment(&self) -> Version {
-        let nv = self.0.checked_add(1).expect("We hit the max version!");
+        let nv = unsafe {
+            NonZeroU64::new_unchecked(
+                self.0
+                    .get()
+                    .checked_add(1)
+                    .expect("We hit the max version!"),
+            )
+        };
         Version(nv)
+    }
+}
+
+impl Default for Version {
+    fn default() -> Self {
+        Self::MIN
     }
 }
