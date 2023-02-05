e2e8aab6a91618eee0c13667d2b84d32911f4978
Austin Hicks <ahicks@ahicks.io>
2023-02-05 13:04:00 -0800

    physics: introduce MortonPrefix, which can be used to calculate quadtree paths

--- crates/physics/src/morton.rs
+++ crates/physics/src/morton.rs
@@ -6,6 +6,20 @@ pub struct MortonCode {
     data: u32,
 }
 
+/// A morton prefix combines two morton codes, and represents the common prefix between them.
+///
+/// For example, if the morton codes are the corners of a box, their prefix is the path in a quadtree which leads to the
+/// smallest node completely containing that box.
+///
+/// Our prefixes are arrays of (0..4) values, where the high bit is set if the y bit in the morton code is nonzero, and
+/// the low bit is set if the x coordinate is nonzero.  We never work in or return just one bit.
+pub struct MortonPrefix {
+    code: u32,
+
+    /// Index of the first common bit in the integer, starting from the least significant. AN empty prefix is 32.  A full prefix is 0.
+    first_valid_bit: u8,
+}
+
 /// Expand a u16 to `...-3-2-1-0` where `-` means unset bit.
 const fn expand_u16(x: u16) -> u32 {
     // - - 2 1
@@ -70,6 +84,51 @@ impl MortonCode {
     }
 }
 
+impl MortonPrefix {
+    pub fn from_code(code: MortonCode) -> MortonPrefix {
+        MortonPrefix {
+            code: code.data,
+            // Right now, it's the whole thing.
+            first_valid_bit: 0,
+        }
+    }
+
+    /// Merge this prefix with another prefix, producing the prefix which is the prefix of both prefixes.
+    #[must_use = "This returns a new prefix"]
+    pub fn merge(&self, other: MortonPrefix) -> MortonPrefix {
+        // We want the first varying bit from the top, which we then invert into the first equal bit from the bottom.
+        let xored = self.code ^ other.code;
+        // First bit which varies, from the top.  Note that trailing zeros is incorrect: we know only that the prefixes
+        // are the same, but the low bits may have more than one varying bit.
+        let top_varying = xored.leading_zeros();
+        // So flip it: 32 - top_varying is the index of the lowest bit which varies, 0 if no bits were set.
+        let bottom_varying = 32 - top_varying;
+        // the actual thing we want is the maximum of all 3, the highest bit which is the same accounting for already valid parts of the prefixes.
+        let candidate = bottom_varying
+            .max(self.first_valid_bit as u32)
+            .max(other.first_valid_bit as u32);
+        // We now bump candidate up to the next multiple of 2, since we never deal in fractional bits.
+        let actual = (candidate + 1) & (!1);
+        MortonPrefix {
+            code: self.code,
+            first_valid_bit: actual.try_into().unwrap(),
+        }
+    }
+
+    /// Unpack this prefix into an iterator of `u8` of the form `yx` where y and x aree bits whicha re set if the
+    /// appropriate bit is set in the prefix.
+    pub fn unpack(&self) -> impl Iterator<Item = u8> {
+        let num_elems = (32 - self.first_valid_bit) / 2;
+        let mask = 0xc0000000;
+        let mut code = self.code;
+        (0..num_elems).map(move |_| {
+            let item = (code & mask) >> 30;
+            code <<= 2;
+            item.try_into().unwrap()
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -126,6 +185,70 @@ mod tests {
             let complicated = MortonCode::encode(x, y).as_quadrants();
             let boring = boring_quadrant_computation(x, y);
             assert_eq!(complicated, boring);
+        }
+    }
+
+    #[track_caller]
+    fn test_prefix((x1, y1): (u16, u16), (x2, y2): (u16, u16), expected: &[u8]) {
+        let code1 = MortonCode::encode(x1, y1);
+        let code2 = MortonCode::encode(x2, y2);
+        let prefix = MortonPrefix::from_code(code1).merge(MortonPrefix::from_code(code2));
+        let got = prefix.unpack().collect::<Vec<_>>();
+        assert_eq!(&got[..], expected);
+    }
+
+    #[test]
+    fn test_prefixes_basic() {
+        assert_eq!(
+            MortonPrefix::from_code(MortonCode::encode(0xffff, 0xffff))
+                .unpack()
+                .collect::<Vec<_>>(),
+            vec![3; 16]
+        );
+
+        test_prefix(
+            (0xffff, 0xffff),
+            (0xffff, 0xffff),
+            &[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        );
+
+        test_prefix(
+            (0xff00, 0xff00),
+            (0xffff, 0xffff),
+            &[3, 3, 3, 3, 3, 3, 3, 3],
+        );
+
+        test_prefix((0xffff, 0xffff), (0, 0), &[]);
+    }
+
+    fn boring_morton_prefix((x1, y1): (u16, u16), (x2, y2): (u16, u16)) -> Vec<u8> {
+        let m1 = MortonCode::encode(x1, y1).as_quadrants();
+        let m2 = MortonCode::encode(x2, y2).as_quadrants();
+        m1.into_iter()
+            .zip(m2.into_iter())
+            .take_while(|(x, y)| x == y)
+            .map(|i| i.0)
+            .collect()
+    }
+
+    proptest::proptest! {
+        #![proptest_config(proptest::prelude::ProptestConfig {
+            cases: 10000000,
+            ..Default::default()
+        })]
+        #[test]
+        fn test_prefixes_fuzz(
+            x1: u16,
+            y1: u16,
+            x2: u16,
+            y2: u16,
+        ) {
+            let p1 = MortonPrefix::from_code(MortonCode::encode(x1, y1));
+            let p2 = MortonPrefix::from_code(MortonCode::encode(x2, y2));
+            let merged = p1.merge(p2);
+            let unpacked = merged.unpack().collect::<Vec<_>>();
+            let expected = boring_morton_prefix((x1, y1), (x2, y2));
+            assert_eq!(unpacked, expected);
         }
     }
 }
