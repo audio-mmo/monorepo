e36d8c70df019ebbdde7ce6bbfdea28b31471928
Austin Hicks <ahicks@ahicks.io>
2023-02-11 18:37:50 -0800

    Drop some more unused/to-be-replaced crates

--- crates/client/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod client;
-pub mod frontend_service_provider;
-pub mod main_thread;
-pub mod ui_elements;
-pub mod ui_stack;
-pub mod world_state;
-
-pub use client::*;
-pub use main_thread::*;
--- crates/client/src/ui_elements/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod simple_menu;
-pub mod ui_element_trait;
-
-pub use simple_menu::*;
-pub use ui_element_trait::*;
