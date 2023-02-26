221f008908a186e1e4406d70760dc73cbd9b9b91
Austin Hicks <ahicks@ahicks.io>
2023-02-26 11:33:22 -0800

    AssetStore: remove localization
    We will put this higher up.  We must manage catalogs differently depending on what it is by enough
    that it is worth just doing it on a case-by-case basis (for example deserializing messages
    requires supporting relative paths but once they are deserialized they are already absolute).

--- crates/asset_store/src/asset_store.rs
+++ crates/asset_store/src/asset_store.rs
@@ -1,60 +1,21 @@
-use std::cell::RefCell;
 use std::io::{Read, Result as IoResult};
-use std::marker::PhantomData;
 
 use camino::Utf8PathBuf;
 
 use crate::backing_stores::*;
-use crate::Catalog;
 
-#[derive(Debug, thiserror::Error)]
-pub enum AssetStoreOpenError {
-    #[error("Io: {0}")]
-    Io(#[from] std::io::Error),
-
-    #[error("Unable to parse language tag: {0}")]
-    LanguageTagParseError(#[from] language_tags::ParseError),
-
-    #[error("Specified language tag is invalid: {0}")]
-    LanguageTagValidationError(#[from] language_tags::ValidationError),
-}
-
-pub struct AssetStore<C: Catalog> {
+pub struct AssetStore {
     backing_store: BackingStore,
-    _phantom: PhantomData<*const C>,
 }
 
-impl<C: Catalog> AssetStore<C> {
-    pub fn open_fs(
-        root: Utf8PathBuf,
-        locale_tag: &str,
-    ) -> Result<AssetStore<C>, AssetStoreOpenError> {
-        let parsed = language_tags::LanguageTag::parse(locale_tag)?;
-        parsed.validate()?;
-
+impl AssetStore {
+    pub fn open_fs(root: Utf8PathBuf) -> std::io::Result<AssetStore> {
         let backing_store = BackingStore::new_filesystem(root)?;
-        Ok(AssetStore {
-            backing_store,
-            _phantom: PhantomData,
-        })
+        Ok(AssetStore { backing_store })
     }
 
-    /// This is the worst implementation of open possible: if the catalog is localized, then pretend we want en-us.
-    pub fn open(&self, catalog: &C, key: &str) -> IoResult<Box<dyn Read>> {
-        thread_local!(
-            static WORKING_BUF: RefCell<Utf8PathBuf> = RefCell::new(Utf8PathBuf::new());
-        );
-
-        WORKING_BUF.with(|working_buf| {
-            let mut working_buf = working_buf.borrow_mut();
-            working_buf.clear();
-            working_buf.push(catalog.get_subdirectory());
-            if catalog.is_localized() {
-                working_buf.push("en-us");
-            }
-            working_buf.push(key);
-            self.backing_store.open(working_buf.as_str())
-        })
+    pub fn open(&self, key: &str) -> IoResult<Box<dyn Read>> {
+        self.backing_store.open(key)
     }
 }
 
@@ -62,32 +23,13 @@ impl<C: Catalog> AssetStore<C> {
 mod tests {
     use super::*;
 
-    #[derive(Debug, Eq, Ord, PartialEq, PartialOrd, Hash)]
-    enum TestCatalog {
-        Localized,
-        Unlocalized,
-    }
-
-    impl Catalog for TestCatalog {
-        fn get_subdirectory(&self) -> &str {
-            match self {
-                Self::Localized => "localized",
-                Self::Unlocalized => "unlocalized",
-            }
-        }
-
-        fn is_localized(&self) -> bool {
-            matches!(self, Self::Localized)
-        }
-    }
-
-    fn get_test_store() -> AssetStore<TestCatalog> {
+    fn get_test_store() -> AssetStore {
         let fspath = format!("{}/test_assets/test_catalog", env!("CARGO_MANIFEST_DIR"));
-        AssetStore::open_fs(fspath.into(), "en-us").expect("Could not open the asset store")
+        AssetStore::open_fs(fspath.into()).expect("Could not open the asset store")
     }
 
-    fn read_to_string(store: &AssetStore<TestCatalog>, catalog: &TestCatalog, key: &str) -> String {
-        let mut opened = store.open(catalog, key).unwrap();
+    fn read_to_string(store: &AssetStore, key: &str) -> String {
+        let mut opened = store.open(key).unwrap();
         let mut out = String::new();
         opened.read_to_string(&mut out).unwrap();
         out
@@ -96,13 +38,6 @@ mod tests {
     #[test]
     fn test_basic_reading() {
         let store = get_test_store();
-        assert_eq!(
-            read_to_string(&store, &TestCatalog::Unlocalized, "unlocalized_data.txt"),
-            "unlocalized"
-        );
-        assert_eq!(
-            read_to_string(&store, &TestCatalog::Localized, "localized_data.txt"),
-            "localized=en-us"
-        );
+        assert_eq!(read_to_string(&store, "folder/data.txt"), "hello");
     }
 }
--- crates/asset_store/src/lib.rs
+++ crates/asset_store/src/lib.rs
@@ -1,6 +1,4 @@
 mod asset_store;
 mod backing_stores;
-mod catalog;
 
 pub use asset_store::*;
-pub use catalog::*;
